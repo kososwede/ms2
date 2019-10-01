f112dafe1de666913c5e03159ec4e8b20227f176
Christopher McIlgrew <chrismcilgrew@icloud.com>
2019-10-01 19:26:52 +0000

    added audio functions

--- script/script.js
+++ script/script.js
@@ -1,15 +1,33 @@
+class AudioController {
+    constructor() {
+        this.matchSound = new Audio('sounds/applause5.wav');
+        this.victorySound = new Audio('sounds/victory.wav');
+        this.gameOverSound = new Audio('sounds/gameover.wav');
+     }
+
+}
+
+
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+    });
+  });
+  cards.forEach(card => {
+      card.addEventListener('click', () =>{
+     });
+  });
+}
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready);
 } else {
     ready();
 }
 
-function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
- });
-  });
-}
+let audioController = new AudioController();
+
